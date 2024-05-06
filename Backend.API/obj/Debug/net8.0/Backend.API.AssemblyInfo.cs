806abf11c704473d2945f0290b4e5d2c5bd38935
wendel filipe <wendelfilipe1997@hotmail.com>
2024-05-06 12:42:35 -0300

    update backend

--- Backend.API/obj/Debug/net8.0/Backend.API.AssemblyInfo.cs
+++ Backend.API/obj/Debug/net8.0/Backend.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5213c9ddcc20c20da70b341e6f6909d0cb2eeb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07eb44916f342633c41483907d41ec83ad23808")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
