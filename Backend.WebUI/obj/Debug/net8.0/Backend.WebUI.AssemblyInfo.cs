1ce905f1afe0168a2b14b8aa76a41a528230fe8d
wendel filipe <wendelfilipe1997@hotmail.com>
2024-05-08 17:48:13 -0300

    update feature

--- Backend.WebUI/obj/Debug/net8.0/Backend.WebUI.AssemblyInfo.cs
+++ Backend.WebUI/obj/Debug/net8.0/Backend.WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66fe6d2d5719af6b100f28b5fef3dc2b3b719011")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de8a59a9d7a0ded9e3f4450bc54fb49b566be43d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
