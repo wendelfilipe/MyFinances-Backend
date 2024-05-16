4f9148400660a15b1962f2e160f4fac3f4acb063
wendel filipe <wendelfilipe1997@hotmail.com>
2024-05-16 10:44:54 -0300

    add gitigonore

--- Backend.Domain/obj/Debug/net8.0/Backend.Domain.AssemblyInfo.cs
+++ Backend.Domain/obj/Debug/net8.0/Backend.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8e621b7e80550cfbad4753a2be51c20fa93422c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d5d5236f6996137fc3116a2313194a0a5a3e18d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
