01be31a04d24d4e88aa59a3fa6e57e81f441355a
wendel filipe <wendelfilipe1997@hotmail.com>
2024-07-16 22:08:46 -0300

    update

--- Backend.Infra.Ioc/obj/Debug/net8.0/Backend.Infra.Ioc.AssemblyInfo.cs
+++ Backend.Infra.Ioc/obj/Debug/net8.0/Backend.Infra.Ioc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Infra.Ioc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd381682ef156e158b9b76c674b800110b15480d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a553c40e113cfa256f7e7ab56632d17c1b9e495c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Infra.Ioc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Infra.Ioc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
