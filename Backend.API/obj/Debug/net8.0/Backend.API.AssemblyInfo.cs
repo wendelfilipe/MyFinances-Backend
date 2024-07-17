34b35b32c84872ec27345a9f4ae3dff81fc94646
wendel filipe <wendelfilipe1997@hotmail.com>
2024-07-16 22:21:18 -0300

    .gitignore

--- Backend.API/obj/Debug/net8.0/Backend.API.AssemblyInfo.cs
+++ Backend.API/obj/Debug/net8.0/Backend.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98686f6713d254191a5e9e9346a60a29eff62d6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad87039e0f244c1308fd4750fe7cd2f63f442480")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
